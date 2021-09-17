0c03153418790b348862aa58e4cdfa0d93216d6c
Pinkesh <pinkeshraghuvanshi2312@gmail.com>
2021-09-17 22:18:25 +0530

    made some changes

--- cs251outlab/settings.py
+++ cs251outlab/settings.py
@@ -148,5 +148,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- cs251outlab/urls.py
+++ cs251outlab/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('users.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
